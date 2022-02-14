1f0fca53f41314b081c3bb09d3285ee261b5f41c
Luis Lanca <luis.lanca@hiplatform.com>
2022-02-14 14:31:24 -0300

    Create audio player
    Signed-off-by: Luis Lanca <luis.lanca@hiplatform.com>

--- js/radio.js
+++ js/radio.js
@@ -1,11 +1,63 @@
-function playSound(player, currentIcon, newIcon) {
-  var Icon = document.getElementById(player);
+function $(id) {
+  return document.getElementById(id);
+}
 
-  if (Icon.classList.contains(currentIcon)) {
-    Icon.classList.remove(currentIcon);
-    Icon.classList.add(newIcon);
+const media = document.getElementById("audio");
+
+let ui = {
+  play: "playAudio",
+  audio: "audio",
+  percentage: "percentage",
+  seekObj: "seekObj",
+  currentTime: "currentTime",
+  duration: "duration",
+};
+
+function togglePlay() {
+  if (media.paused === false) {
+    media.pause();
+    $(ui.play).classList.remove("pause");
   } else {
-    Icon.classList.remove(newIcon);
-    Icon.classList.add(currentIcon);
+    media.play();
+    $(ui.play).classList.add("pause");
   }
 }
+
+function calculatePercentPlayed() {
+  let percentage = (media.currentTime / media.duration).toFixed(2) * 100;
+  $(ui.percentage).style.width = `${percentage}%`;
+}
+
+function calculateCurrentValue(currentTime) {
+  const currentMinute = parseInt(currentTime / 60) % 60;
+  const currentSecondsLong = currentTime % 60;
+  const currentSeconds = currentSecondsLong.toFixed();
+  const currentTimeFormatted = `${
+    currentMinute < 10 ? `0${currentMinute}` : currentMinute
+  }:${currentSeconds < 10 ? `0${currentSeconds}` : currentSeconds}`;
+
+  return currentTimeFormatted;
+}
+
+function initProgressBar() {
+  const currentTime = calculateCurrentValue(media.currentTime);
+  $(ui.currentTime).innerHTML = currentTime;
+  $(ui.duration).innerHTML = calculateCurrentValue(media.duration);
+  $(ui.seekObj).addEventListener("click", seek);
+
+  media.onended = () => {
+    $(ui.play).classList.remove("pause");
+    $(ui.percentage).style.width = 0;
+    $(ui.currentTime).innerHTML = "00:00";
+  };
+
+  function seek(e) {
+    const percent = e.offsetX / this.offsetWidth;
+    media.currentTime = percent * media.duration;
+  }
+
+  calculatePercentPlayed();
+}
+
+$(ui.play).addEventListener("click", togglePlay);
+$(ui.audio).addEventListener("timeupdate", initProgressBar);
